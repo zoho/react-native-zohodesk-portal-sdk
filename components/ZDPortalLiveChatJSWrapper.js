89d10447dd3c5225dc498ddb17f27b1ada45c7b7
gokul-11397 <gokula.priyan@zohocorp.com>
2024-09-11 11:14:11 +0530

    new setup changes done in android

--- components/ZDPortalLiveChatJSWrapper.js
+++ components/ZDPortalLiveChatJSWrapper.js
@@ -5,5 +5,28 @@ module.exports = {
   show: function ()
   {
       RNZDPortalLiveChat.show();
+    },
+
+    showGC: function() {
+      RNZDPortalLiveChat.showGC();
+    },
+    showAnswerBot : function() {
+      RNZDPortalLiveChat.showAnswerBot();
+    },
+    showBM : function() {
+      RNZDPortalLiveChat.showBM();
+    },
+    setGCSessionVariable: function(sessionVariable) {
+      RNZDPortalLiveChat.setGCSessionVariable(sessionVariable);
+    },
+    updateGCSessionVariable : function(sessionVariable) {
+      RNZDPortalLiveChat.updateGCSessionVariable(sessionVariable);
+    },
+    setBMSessionVariable : function(sessionVariable) {
+      RNZDPortalLiveChat.setBMSessionVariable(sessionVariable);
+    },
+    updateBMSessionVariable: function(sessionVariable) {
+      RNZDPortalLiveChat.updateBMSessionVariable(sessionVariable);
     }
+
 }
--- components/ZDPortalTicketsJSWrapper.js
+++ components/ZDPortalTicketsJSWrapper.js
@@ -4,5 +4,12 @@ module.exports = {
 
     show: function() {
       RNZDPortalTickets.show();
+    },
+    getTicketForm: function(params,flags,successCallback,errorCallback) {
+      RNZDPortalTickets.getTicketForm(params,flags,successCallback,errorCallback);
+    },
+    getTicketFields: function(params,flags,successCallback,errorCallback) {
+      RNZDPortalTickets.getTicketFields(params,flags,successCallback,errorCallback);
     }
+
 }
