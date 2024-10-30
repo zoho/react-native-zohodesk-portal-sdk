ae7cf7032de2057c9e5b3b1fff84202a678d0264
rajesh.jesh <rajesh.jesh@zohocorp.com>
2024-10-30 12:30:21 +0530

    added kb configuration for android

--- zohodesk-portal-kb/android/src/main/java/com/zohodeskportalkb/RNZohoDeskPortalKbModule.kt
+++ zohodesk-portal-kb/android/src/main/java/com/zohodeskportalkb/RNZohoDeskPortalKbModule.kt
@@ -3,7 +3,10 @@ package com.zohodeskportalkb
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableMap
 import com.zoho.desk.asap.kb.ZDPortalKB;
+import com.zoho.desk.asap.kb.utils.ZDPKBConfiguration
+import com.zohodeskportalapikit.Converter.getBooleanOrDefault
 
 class RNZohoDeskPortalKbModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -25,6 +28,17 @@ class RNZohoDeskPortalKbModule(reactContext: ReactApplicationContext) :
   @ReactMethod
   fun showCategory(withPermalink: String){
     ZDPortalKB.showCategoryWithPermalink(currentActivity, withPermalink)
+  }
+
+  @ReactMethod
+  fun setConfiguration(config: ReadableMap){
+    val configuration = ZDPKBConfiguration.Builder()
+      .disableArticleLike(config.getBooleanOrDefault("disableArticleLike",default = false))
+      .disableArticleDislike(config.getBooleanOrDefault("disableArticleDislike",default = false))
+      .isArticleDetailSearchAllowed(!config.getBooleanOrDefault("disableArticleDetailSearch",default = false))
+      .build()
+    
+    ZDPortalKB.setConfiguration(configuration)
   }
 
 }
